033055d5f3a6f85b5eb24223788d81c5cc44e70c
su <673399718@qq.com>
2022-10-22 14:55:55 +0100

    react micro-services

---
+++ app-blogs/src/Constant.js
@@ -0,0 +1,20 @@
+export const arrBlogs = [
+    {
+      "blogID": 1,
+      "blogName": "Front-End Technical Interview Guide",
+      "blogDetail": "How to Prepare for React Interviews",
+      "blogURL": "https://www.freecodecamp.org/news/prepare-for-react-technical-interviews/"
+    },
+    {
+      "blogID": 2,
+      "blogName": "The Complete Guide to Full Stack Web3 Development",
+      "blogDetail": "Building a full stack web3 app with Next.js, Polygon, Solidity, The Graph, IPFS, and Hardhat",
+      "blogURL": "https://dev.to/edge-and-node/the-complete-guide-to-full-stack-web3-development-4g74"
+    },
+    {
+      "blogID": 3,
+      "blogName": "Algorithms in JavaScript with visual examples.",
+      "blogDetail": "Discover a set of javascript best coding practices",
+      "blogURL": "https://dev.to/swastikyadav/algorithms-in-javascript-with-visual-examples-gh3"
+    }
+  ]
